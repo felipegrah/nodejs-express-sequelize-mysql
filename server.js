3ab0bf8758cd9f2150c806297eed40ea513915bf
Felipe <felipegrah@hotmail.com>
2020-10-11 11:08:04 -0300

    Authentication jwt

--- server.js
+++ server.js
@@ -5,9 +5,11 @@ const cors = require("cors");
 const app = express();
 
 const db = require("./app/models");
+const Role = db.role;
 
 db.sequelize.sync({ force: true }).then(() => {
   console.log("Drop and re-sync db.");
+  initial();
 });
 
 var corsOptions = {
@@ -27,6 +29,8 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to bezkoder application." });
 });
 
+require('./app/routes/auth.routes')(app);
+require('./app/routes/user.routes')(app);
 require("./app/routes/turorial.routes")(app);
 
 // set port, listen for requests
@@ -35,3 +39,20 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
+
+function initial() {
+  Role.create({
+    id: 1,
+    name: "user"
+  });
+
+  Role.create({
+    id: 2,
+    name: "moderator"
+  });
+
+  Role.create({
+    id: 3,
+    name: "admin"
+  });
+}
